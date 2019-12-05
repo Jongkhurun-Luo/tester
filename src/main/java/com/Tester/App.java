f3f39805b63c8cbc6d2a4985da16abbf1a58d2f4
jongkhurun <jongkhurun@192.168.1.103>
2019-12-05 11:28:11 +0800

    test

---
+++ src/main/java/com/Tester/App.java
@@ -0,0 +1,11 @@
+package com.Tester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Mac Eclipse Maven Git Selenium Safari");
+	}
+}
